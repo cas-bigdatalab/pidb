62dde53018a9fcd72e9478de792a830be12c9396
lzxddz <lzxddz@qq.com>
2019-02-15 19:43:13 +0800

    add configure file

--- ai-modules-scala/src/main/scala/cn/aipm/service/Services.scala
+++ ai-modules-scala/src/main/scala/cn/aipm/service/Services.scala
@@ -1,15 +1,39 @@
 package cn.aipm.service
 
-import scala.collection.immutable.Map
 import java.io.InputStream
+import java.util.Properties
+import java.io.BufferedInputStream
+import java.io.FileInputStream
+import scala.collection.mutable.HashMap
+
+import scala.collection.immutable.Map
 import scala.util.parsing.json.JSON
 
 
 object Services {
-  var hostUrl = "http://10.0.86.128:8081/service/"
+
+  val configures = {
+    val filePath =System.getProperty("user.dir") + "/ai-services.conf"
+    val prop = new Properties()
+    val configures = HashMap[String,String]()
+    try {
+      val in = new BufferedInputStream(new FileInputStream(filePath))
+      prop.load(in) ///加载属性列表
+      val properties = prop.propertyNames()
+      while(properties.hasMoreElements){
+        val k = properties.nextElement().toString
+        configures.put(k, prop.getProperty(k))
+      }
+      in.close()
+    } catch {
+      case e: Exception =>
+        System.out.println(e)
+    }
+    configures
+  }
 
   def computeFaceSimilarity(img1InputStream:InputStream,img2InputStream:InputStream): Double={
-    val serviceUrl = hostUrl + "face/similarity/"
+    val serviceUrl = configures("FaceSimilarityServiceUrl")
 
     val contents = Map("image1" -> img1InputStream, "image2" -> img2InputStream)
 
@@ -26,7 +50,7 @@ object Services {
   }
 
   def extractPlateNumber(img1InputStream:InputStream): String= {
-    val serviceUrl = hostUrl + "plate/"
+    val serviceUrl = configures("PlateNumberServiceUrl")
 
     val contents = Map("image1" -> img1InputStream)
 
