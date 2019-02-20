25f5a3cddb87d585e6264e45e895704bee6f2128
bluejoe2008@gmail.com <bluejoe2008@gmail.com>
2019-02-20 10:31:27 +0800

    cypher plugins

--- ai-modules-scala/src/main/scala/cn/aipm/image/DogOrCatClassifier.scala
+++ ai-modules-scala/src/main/scala/cn/aipm/image/DogOrCatClassifier.scala
@@ -8,7 +8,7 @@ import cn.pidb.util.{Config, ConfigEx}
 class DogOrCatClassifier extends PropertyExtractor {
   var aipmHttpHostUrl = "http://127.0.0.1/"
 
-  override def declareProperties() = Map("Animal" -> classOf[String])
+  override def declareProperties() = Map("animal" -> classOf[String])
 
   override def extract(x: Any): Map[String, Any] = x.asInstanceOf[Blob].offerStream(is => {
     val animal = Services.initialize(aipmHttpHostUrl).classifyAnimal(is)
